ee9e650655c7d150741438f573436cb9b0eace5c
Manuel Izurieta <manuel@aaa.aaa>
2017-12-18 22:04:41 -0300

    Initial working release

--- src/main/java/com/test/rest/App.java
+++ src/main/java/com/test/rest/App.java
@@ -1,13 +1,36 @@
 package com.test.rest;
 
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClientException;
+
+import com.test.rest.controllers.ConsumerControllerClient;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws RestClientException, IOException 
     {
         System.out.println( "Hello World!" );
+		ApplicationContext ctx = SpringApplication.run(App.class, args);
+		
+		ConsumerControllerClient consumerControllerClient=ctx.getBean(ConsumerControllerClient.class);
+		System.out.println(consumerControllerClient);
+		consumerControllerClient.getEmployee();        
     }
+    
+	@Bean
+	public  ConsumerControllerClient  consumerControllerClient()
+	{
+		return  new ConsumerControllerClient();
+	}    
+    
 }
