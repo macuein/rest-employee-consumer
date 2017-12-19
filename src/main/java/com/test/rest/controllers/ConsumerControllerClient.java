0e0ed380d22cb4957af1aaebbfbaf9333db04690
Manuel Izurieta <manuel@aaa.aaa>
2017-12-19 00:26:27 -0300

    employee-consumer integrated with Eureka

--- src/main/java/com/test/rest/controllers/ConsumerControllerClient.java
+++ src/main/java/com/test/rest/controllers/ConsumerControllerClient.java
@@ -1,31 +1,49 @@
 package com.test.rest.controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+@Controller
 public class ConsumerControllerClient 
 {
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	public void getEmployee() throws RestClientException, IOException 
 	{
 
-		String baseUrl = "http://localhost:8080/employee";
+		//String baseUrl = "http://localhost:8080/employee";
+		
+		List<ServiceInstance> instances = discoveryClient.getInstances("rest-employee-producer");
+		ServiceInstance serviceInstance = instances.get(0);
+		String baseUrl = serviceInstance.getUri().toString();
+		baseUrl = baseUrl+"/employee";
+		
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response=null;
+		
 		try
 		{
-			response=restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(),String.class);
+			response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(),String.class);
 		}
 		catch (Exception ex)
 		{
 			System.out.println(ex);
 		}
+		
 		System.out.println(response.getBody());
 	}
 
